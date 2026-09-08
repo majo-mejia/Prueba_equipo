a093a6d6638adbf24ff7626034b824f7d847cfc3
jhzz-8 <juanj.juradoc@uqvirtual.edu.co>
2026-09-07 19:57:47 -0500

    cambio

--- src/main/java/co/edu/uniquindio/poo/Main.java
+++ src/main/java/co/edu/uniquindio/poo/Main.java
@@ -8,6 +8,7 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("hola xd");
         System.out.println("Hola Soy Juan Jurado Ch");
+        System.out.println("hai");
 
     }
 }
